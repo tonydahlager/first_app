ef311eee7bf186d91d74935ee33059217165c4c0
Weston Platter <westonplatter@gmail.com>
2014-02-13 21:36:57 -0700

    question for tony about :resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 FirstApp::Application.routes.draw do
+  
   get "welcome/index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,8 +9,11 @@ FirstApp::Application.routes.draw do
 
   # TD QUESTION 2/13/14
   # Where do routes exist in the path of the HTML request in relation to the controller?
-  
   resources :posts
+  
+  # TONY
+  # What is the long hand way to write all the resource routes?
+    
 
   root to: "welcome#index"
 
